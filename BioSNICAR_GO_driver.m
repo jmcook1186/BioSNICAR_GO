0fc5494642540bccd2e57987e561fa51eb47d642
Unknown <joe.cook@sheffield.ac.uk>
2018-05-24 11:32:43 +0100

    User choice between Mie or GO
    Updated driver so that user can easily choose between running Mie or GO ice grain optics. Curently Mie available in range 10 - 3000 microns (1um resoluton) and GO available from 3000 - 30000 microns (1000 micron resolution) in both side length and depth.

--- BioSNICAR_GO_driver.m
+++ BioSNICAR_GO_driver.m
@@ -1,5 +1,28 @@
-% Driver routine for BioSNICAR_GO. Calls function in
-% snicar8d_geom_optics.m
+% Driver routine for BioSNICAR_GO.
+% Written by Joseph Cook, 2018 (joe.cook@sheffield.ac.uk)
+
+% This code adds significant functionality to the original SNICAR model
+% (Flanner et al, 2007) by enabling users to model large, aspherical ice
+% grains as well as small, spherical ones as per SNICAR. The model also
+% offers incorporation of biological impurities as per BioSNICAR (Cook et
+% al., 2017).
+
+% The user chooses to access ice grains whose optical properties are
+% calculated using Mie scattering calculations (assuming grains are small
+% and spherical - good for snow) or hexagonal plates/columns of arbitrarily
+% large dimensions using geometric optics (good for wet snow/ice).
+
+% If the user chooses to use Mie scattering, the ice grain effective radii
+% should then be defined under the variable name rds_snw for each vertical
+% layer. 
+
+% if the user chooses to use geometric optics, the ice grains dimensions 
+% are described using the length of one side of the hexagonal face
+% (side_length) and the column depth (depth). If the aspect ratio is less
+% than 1 this is a hexagnal plate, if the aspect ratio is greater than one
+% it is a hexagonal column. From these user-defined dimensions, the volume,
+% surface area and apotherm of the hexagonal ice grains are calculated and
+% reported to the command window.
 
 %%%%%%%%%%  Input parameters: %%%%%%%%%%%
 
@@ -22,52 +45,67 @@
 % dz:           array of snow layer thicknesses [meters]. Top layer has index 1. 
 %                  The length of this array defines number of snow layers
 % rho_snw:      array of snow layer densities [kg m-3]. Must have same length as dz
-% side_length:  length of one side of hexagonal plane of ice crystal [microns]. Must have same length as dz
-% depth:        vertical dimension of hexagonal columnar ice crystal [microns]. Must have same lengths as dz  
+% rds_snw:      effective radii for ice grains. Only used for Mie option
+% side_length:  length of one side of hexagonal plane of ice crystal [microns]. Only used for GO option
+% depth:        vertical dimension of hexagonal columnar ice crystal [microns]. Only used for GO option  
 % nbr_aer:      number of aerosol species in snowpack
 % mss_cnc_X:    mass mixing ratio of aerosol X
 % fl_X:         name of file containing optical properties for aerosol X
 
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear;
+
+
+
+%%%%%%%%%%%%%%%%%%% USER DEFNED INPUTS HERE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 
 % RADIATIVE TRANSFER CONFIGURATION:
 BND_TYP  = 1;        % 1= 470 spectral bands
 DIRECT   = 1;        % 1= Direct-beam incident flux, 0= Diffuse incident flux
 APRX_TYP = 1;        % 1= Eddington, 2= Quadrature, 3= Hemispheric Mean
 DELTA    = 1;        % 1= Apply Delta approximation, 0= No delta
+coszen   = 0.50;     % if DIRECT give cosine of solar zenith angle 
 
-% COSINE OF SOLAR ZENITH ANGLE FOR DIRECT-BEAM
-coszen   = 0.50;
+% THICKNESSES OF EACH VERTICAL LAYER(array) (units: meters):
+dz       = [0.005 0.3 0.5 0.5 0.5];
+nbr_lyr  = length(dz);  % number of snow layers
 
 % REFLECTANCE OF SURFACE UNDERLYING SNOW:
 %   Value is applied to all wavelengths.
-%   User can also specify spectrally-dependent ground albedo
-%   internally in snicar8d_geom_optics.m
 R_sfc    = 0.15;
 
-% THICKNESSES OF EACH VERTICAL LAYER(array) (units: meters):
-dz       = [0.1 0.2 0.2 0.2 0.5];
- 
-nbr_lyr  = length(dz);  % number of snow layers
-
 % DENSITY OF EACH VERTICAL LAYER (units: kg/m3)
-rho_snw(1:nbr_lyr) = [700, 700, 700, 700, 700];  
+rho_snw(1:nbr_lyr) = [600, 600, 600, 600, 600]; 
 
-% DIMENSIONS OF ICE CRYSTAL (SIDE LENGTH OF HEAGNAL PLANE AND COLUMN DEPTH (units: microns):
-side_length(1:nbr_lyr) = [20000,20000,20000,20000,20000];
-depth(1:nbr_lyr) = [20000,20000,20000,20000,20000]
+% CHOOSE METHOD FOR DETERMINING OPTICAL PROPERTIES OF ICE GRAINS
+% for small spheres choose Mie, for hexagonal plates or columns of any
+% size, choose GeometricOptics
+Mie = 0;
+GeometricOptics = 1;
+
+%SET ICE GRAIN DIMENSIONS
+% if using Mie optical properties, set rds_snw
+rds_snw = [200,200,200,200,200];
+
+% if using GeometricOptics, set side_length and depth
+side_length(1:nbr_lyr) = [30000,30000,10000,10000,10000]; 
+depth(1:nbr_lyr) = [30000,30000,10000,10000,10000];
 
 % TOTAL NUMBER OF AEROSOL SPECIES IN MODEL
 nbr_aer = 17;
 
 % LOOP FOR LAP MASS MIXING RATIOS IN ICE
-for x = [3e5]   % for reference: 1e3 = 1ug/g (1000 ppb or 1 ppm)
+for x = [1000 100000 200000]   % for reference: 1e3 = 1ug/g (1000 ppb or 1 ppm)
                   % 1 e6 = 1000ug = 1mg
 
 % PARTICLE MASS MIXING RATIOS (units: ng(species)/g(ice), or ppb)
+% add mixing ratio of each particle per vertical layer or add 'x' to 
+% loop through values defined above
+
 mss_cnc_sot1(1:nbr_lyr)  = [0,0,0,0,0];  % uncoated black carbon
 mss_cnc_sot2(1:nbr_lyr)  = [0,0,0,0,0];    % coated black carbon
 mss_cnc_dst1(1:nbr_lyr)  = [0,0,0,0,0];    % dust species 1
@@ -82,9 +120,20 @@ mss_cnc_bio4(1:nbr_lyr)  = [0,0,0,0,0];    % Biological impurity species 4
 mss_cnc_bio5(1:nbr_lyr)  = [0,0,0,0,0];    % Biological impurity species 5
 mss_cnc_bio6(1:nbr_lyr)  = [0,0,0,0,0];    % Biological impurity species 6
 mss_cnc_bio7(1:nbr_lyr)  = [0,0,0,0,0];    % Biological impurity species 7
-mss_cnc_RBio1(1:nbr_lyr) = [0,0,0,0,0]; % Realistic Cell (measured pigments, 20 micron diameter)
+mss_cnc_RBio1(1:nbr_lyr) = [x,0,0,0,0]; % Realistic Cell (measured pigments, 20 micron diameter)
 mss_cnc_hematite(1:nbr_lyr) = [0,0,0,0,0];   % hematite
 mss_cnc_mixed_sand(1:nbr_lyr) = [0,0,0,0,0];  % mixed sand
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
 
 
 % FILE NAMES CONTAINING MIE PARAMETERS FOR ALL AEROSOL SPECIES:
@@ -106,97 +155,129 @@ fl_RBio1 = 'Real_Bio_1.nc'; % Biological impurity with measured pigments (inc pu
 fl_hematite  = 'Hematite.nc'; % Hematite particles
 fl_mixed_sand  = 'Mixed_Sand.nc'; % Mixed sand (quartz and clays)
 
-% call SNICAR with these inputs:
+% Check that one method for ice optical properties is selected, if not
+% raise exception
 
-data_in = snicar8d_geom_optics(BND_TYP, DIRECT, APRX_TYP, DELTA, coszen, R_sfc, ...
-    dz, rho_snw, side_length, depth, nbr_aer, mss_cnc_sot1, ...
-    mss_cnc_sot2, mss_cnc_dst1, mss_cnc_dst2, ...
-    mss_cnc_dst3, mss_cnc_dst4, mss_cnc_ash1, mss_cnc_bio1, mss_cnc_bio2,mss_cnc_bio3,mss_cnc_bio4,mss_cnc_bio5, mss_cnc_bio6, mss_cnc_bio7, mss_cnc_RBio1,mss_cnc_hematite, mss_cnc_mixed_sand, fl_sot1, ...
-    fl_sot2, fl_dst1, fl_dst2, fl_dst3, fl_dst4, fl_ash1, fl_bio1,fl_bio2,fl_bio3,fl_bio4,fl_bio5,fl_bio6, fl_bio7, fl_RBio1, fl_hematite, fl_mixed_sand);
+if GeometricOptics == 1 && Mie ==1 
 
+    error = "You have selected both methods for ice grain optics - please pick one!" 
 
-% process input data:
-% (see description of data_out at the end of snicar8d.m for more info)
-wvl         = data_in(:,1);   % wavelength grid
-albedo      = data_in(:,2);   % spectral albedo
-alb_slr     = data_in(1,3);   % broadband albedo (0.3-5.0um)
-alb_vis     = data_in(2,3);   % visible albedo (0.3-0.7um)
-alb_nir     = data_in(3,3);   % near-IR albedo (0.7-5.0um)
-flx_abs_snw = data_in(4,3);   % total radiative absorption by all snow layers (not including underlying substrate)
+elseif GeometricOptics == 0 && Mie ==0
+    error = "You have not selected a method for ice grain optics, please choose either Mie or Geometric Optics"
 
-flx_abs(1)     = data_in(6,4); % top layer solar absorption
-flx_vis_abs(1) = data_in(7,4); % top layer VIS absorption
-flx_nir_abs(1) = data_in(8,4); % top layer NIR absorption
-heat_rt = data_in([1:5],6); % heating rate per layer K/hr
-F_abs = [data_in(6,4),data_in(9,4),data_in(12,4),data_in(15,4),data_in(18,4)]; % absorbed energy per layer (W/m2)
-intensity2 = data_in([1:470],[7:11]);
-%albedo = smooth(albedo,0.005); % add a simple smoothing function with short period
+else
+    
+    % run snicar in either GO or Mie mode:
+    
+    if GeometricOptics == 1
+        
+        data_in = snicar8d_GO(BND_TYP, DIRECT, APRX_TYP, DELTA, coszen, R_sfc, ...
+            dz, rho_snw, side_length, depth, nbr_aer, mss_cnc_sot1, ...
+            mss_cnc_sot2, mss_cnc_dst1, mss_cnc_dst2, ...
+            mss_cnc_dst3, mss_cnc_dst4, mss_cnc_ash1, mss_cnc_bio1, mss_cnc_bio2,mss_cnc_bio3,mss_cnc_bio4,mss_cnc_bio5, mss_cnc_bio6, mss_cnc_bio7, mss_cnc_RBio1,mss_cnc_hematite, mss_cnc_mixed_sand, fl_sot1, ...
+            fl_sot2, fl_dst1, fl_dst2, fl_dst3, fl_dst4, fl_ash1, fl_bio1,fl_bio2,fl_bio3,fl_bio4,fl_bio5,fl_bio6, fl_bio7, fl_RBio1, fl_hematite, fl_mixed_sand);
+    
+        for i = 1:1:length(dz)
+            "******** REPORTING ICE GRAIN DIMENSIONS ********"
+            Volume(i) = 1.5*sqrt(3)*(side_length(i)^2)*depth(i) % ice grain volume
+            Area_total(i) = 3 * side_length(i) * (sqrt(3)*side_length(i)+depth(i)*2) %total surface area 
+            Area(i) = Area_total(i)/4   % projected area
+            apothem(i) = (2*Area(i)) / (depth(i)*6) % apothem is distance from centre point to midpoint of a side for hexagon
+            diameter(i) = 2*apothem(i) % midpoint of one side to midpoint of opposite side
+        end
+        
+    end
 
-%separate subsurface light field into layers
-sub1 = smooth(intensity2(:,1),4);
-sub2 = smooth(intensity2(:,2),4);
-sub3 = smooth(intensity2(:,3),4);
-sub4 = smooth(intensity2(:,4),4);
-sub5 = smooth(intensity2(:,5),4);
+    if Mie == 1
+        
+    data_in = snicar8d_Mie(BND_TYP, DIRECT, APRX_TYP, DELTA, coszen, R_sfc, ...
+        dz, rho_snw, rds_snw, nbr_aer, mss_cnc_sot1, ...
+        mss_cnc_sot2, mss_cnc_dst1, mss_cnc_dst2, ...
+        mss_cnc_dst3, mss_cnc_dst4, mss_cnc_ash1, mss_cnc_bio1, mss_cnc_bio2,mss_cnc_bio3,mss_cnc_bio4,mss_cnc_bio5, mss_cnc_bio6, mss_cnc_bio7, mss_cnc_RBio1,mss_cnc_hematite, mss_cnc_mixed_sand, fl_sot1, ...
+        fl_sot2, fl_dst1, fl_dst2, fl_dst3, fl_dst4, fl_ash1, fl_bio1,fl_bio2,fl_bio3,fl_bio4,fl_bio5,fl_bio6, fl_bio7, fl_RBio1, fl_hematite, fl_mixed_sand);
+    end
 
-sub1_tot = sum(sub1);
-sub2_tot = sum(sub2);
-sub3_tot = sum(sub3);
-sub4_tot = sum(sub4);
-sub5_tot = sum(sub5);
-sub_tot_line = [sub1_tot,sub2_tot,sub3_tot,sub4_tot,sub5_tot];
-depths = [dz(1),dz(1)+dz(2),dz(1)+dz(2)+dz(3),dz(1)+dz(2)+dz(3)+dz(4), dz(1)+dz(2)+dz(3)+dz(4)+dz(5)];
+    % process input data:   
+    wvl         = data_in(:,1);   % wavelength grid
+    albedo      = data_in(:,2);   % spectral albedo
+    alb_slr     = data_in(1,3);   % broadband albedo (0.3-5.0um)
+    alb_vis     = data_in(2,3);   % visible albedo (0.3-0.7um)
+    alb_nir     = data_in(3,3);   % near-IR albedo (0.7-5.0um)
+    flx_abs_snw = data_in(4,3);   % total radiative absorption by all snow layers (not including underlying substrate)
 
-figure(1);
+    flx_abs(1)     = data_in(6,4); % top layer solar absorption
+    flx_vis_abs(1) = data_in(7,4); % top layer VIS absorption
+    flx_nir_abs(1) = data_in(8,4); % top layer NIR absorption
+    heat_rt = data_in([1:5],6); % heating rate per layer K/hr
+    F_abs = [data_in(6,4),data_in(9,4),data_in(12,4),data_in(15,4),data_in(18,4)]; % absorbed energy per layer (W/m2)
+    intensity2 = data_in([1:470],[7:11]);
+    
+    %albedo = smooth(albedo,0.005); % add a simple smoothing function with short period
+    %separate subsurface light field into layers
+    sub1 = smooth(intensity2(:,1),4);
+    sub2 = smooth(intensity2(:,2),4);
+    sub3 = smooth(intensity2(:,3),4);
+    sub4 = smooth(intensity2(:,4),4);
+    sub5 = smooth(intensity2(:,5),4);
 
-% make a plot of spectrally-resolved albedo:
-plot(wvl,albedo,'linewidth',3);
-xlabel('Wavelength (\mum)','fontsize',20);
-ylabel('Albedo','fontsize',20);
-set(gca,'xtick',0:0.1:5,'fontsize',16);
-set(gca,'ytick',0:0.1:1.0,'fontsize',16);
-xlim([0.3 2.5])
-ylim([0,1])
-grid on;
-hold on;
+    sub1_tot = sum(sub1);
+    sub2_tot = sum(sub2);
+    sub3_tot = sum(sub3);
+    sub4_tot = sum(sub4);
+    sub5_tot = sum(sub5);
+    sub_tot_line = [sub1_tot,sub2_tot,sub3_tot,sub4_tot,sub5_tot];
+    depths = [dz(1),dz(1)+dz(2),dz(1)+dz(2)+dz(3),dz(1)+dz(2)+dz(3)+dz(4), dz(1)+dz(2)+dz(3)+dz(4)+dz(5)];
 
-% plot subsurface light field
+    figure(1);
 
-% figure(2);
-% plot(wvl, sub1, 'DisplayName','0 - 1 cm');
-% plot(wvl, sub2','DisplayName','1 - 3 cm');
-% plot(wvl, sub3,'DisplayName','3 - 5 cm');
-% plot(wvl, sub4,'DisplayName','5 - 7 cm');
-% plot(wvl, sub5,'DisplayName','7 - 9 cm');
-% xlabel('Wavelength (\mum)','fontsize',20);
-% ylabel('Planar Intensity Wm-2','fontsize',20);
-% xlim([0.3 2.5]);
-% ylim([0 0.03]);
-% set(gca,'xtick',0:0.1:5,'fontsize',16);
-% set(gca,'ytick',0:0.01:0.1,'fontsize',16);
-% legend;
-% hold on
-
-%plot total energy intensity against depth
-
-% figure(3);
-% plot(depths,sub_tot_line);
-% xlabel('Depth beneath surface (m)','fontsize',20);
-% ylabel('planar intensity (Wm-2)','fontsize',20);
-% set(gca,'xtick',0:0.01:0.1,'fontsize',16);
-% set(gca,'ytick',0:0.1:1,'fontsize',16);
-% hold on
-
-% report slope between blue and green reflectance
-bgslope = ((albedo(26)-albedo(18))/0.075);
-
-%Report albedo
-alb_slr; % albedo over solar spectrum
-%flx_abs_snw % absorbed energy in the snowpack
-%heat_rt; % radiative heating rate in K/hr
-
-snow_depth = sum(dz); % depth of snowpack incorporating all layers
-temp_grad = ((heat_rt(1) - heat_rt(end)))/snow_depth; % temperature gradient through snowpack
+    % make a plot of spectrally-resolved albedo:
+    plot(wvl,albedo,'linewidth',2);
+    xlabel('Wavelength (\mum)','fontsize',20);
+    ylabel('Albedo','fontsize',20);
+    set(gca,'xtick',0:0.1:5,'fontsize',16);
+    set(gca,'ytick',0:0.1:1.0,'fontsize',16);
+    xlim([0.3 2.5])
+    ylim([0,1])
+    grid on;
+    hold on;
 
 
+    % plot subsurface light field
+
+    % figure(2);
+    % plot(wvl, sub1, 'DisplayName','0 - 1 cm');
+    % plot(wvl, sub2','DisplayName','1 - 3 cm');
+    % plot(wvl, sub3,'DisplayName','3 - 5 cm');
+    % plot(wvl, sub4,'DisplayName','5 - 7 cm');
+    % plot(wvl, sub5,'DisplayName','7 - 9 cm');
+    % xlabel('Wavelength (\mum)','fontsize',20);
+    % ylabel('Planar Intensity Wm-2','fontsize',20);
+    % xlim([0.3 2.5]);
+    % ylim([0 0.03]);
+    % set(gca,'xtick',0:0.1:5,'fontsize',16);
+    % set(gca,'ytick',0:0.01:0.1,'fontsize',16);
+    % legend;
+    % hold on
+
+    %plot total energy intensity against depth
+
+    % figure(3);
+    % plot(depths,sub_tot_line);
+    % xlabel('Depth beneath surface (m)','fontsize',20);
+    % ylabel('planar intensity (Wm-2)','fontsize',20);
+    % set(gca,'xtick',0:0.01:0.1,'fontsize',16);
+    % set(gca,'ytick',0:0.1:1,'fontsize',16);
+    % hold on
+
+    %Report albedo
+    "********* REPORTING BROADBAND ALBEDDO ******* "
+    
+    alb_slr % albedo over solar spectrum
+    flx_abs_snw; % absorbed energy in the snowpack
+    heat_rt; % radiative heating rate in K/hr
+
+    snow_depth = sum(dz); % depth of snowpack incorporating all layers
+    temp_grad = ((heat_rt(1) - heat_rt(end)))/snow_depth; % temperature gradient through snowpack
+
+end
 end
