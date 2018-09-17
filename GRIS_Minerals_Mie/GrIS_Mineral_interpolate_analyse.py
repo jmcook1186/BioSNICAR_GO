2c3a19657fbc106aed40e661b0f7ec2ba6e70dc6
Unknown <joe.cook@sheffield.ac.uk>
2018-09-17 18:15:45 +0100

    Add python script for calculating PSD weights update Mie code
    Final draft codes - need updated annotations

---
+++ GRIS_Minerals_Mie/GrIS_Mineral_interpolate_analyse.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 17 08:57:18 2018
+
+@author: joe
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import interpolate
+from scipy.signal import savgol_filter
+
+
+dftemp = pd.read_csv('/media/joe/C38C-3252/GRIS_dust_K.csv')
+
+f = interpolate.interp1d(dftemp['WL'],dftemp['K'])
+WLnew = np.arange(0.35,2.5,0.001)
+Knew = f(WLnew)
+
+Ksmooth = savgol_filter(Knew, 101, 3) # window size 51, polynomial order 3
+
+df = pd.DataFrame()
+df['WL'] = WLnew
+df['K'] = Ksmooth
+
+plt.figure(figsize=(6,4))
+plt.plot(df['WL'],df['K']),plt.xlabel('Wavelength (microns)',fontsize=16),plt.ylabel('k',fontsize=16)
+plt.xticks(fontsize=16),plt.yticks(fontsize=16)
+plt.savefig('/media/joe/C38C-3252/GrisMineral_k.jpg',dpi=300)
+
+
+
+
+PSD = pd.read_csv('/media/joe/C38C-3252/PSD.csv',header=None)
+
+Weights = [];
+
+Weights.append((PSD[PSD < 0.1 ].count()) /(len(PSD)*100))
+Weights.append((PSD[(PSD > 0.1) & (PSD < 0.2) ].count())/(len(PSD)*100))
+Weights.append((PSD[(PSD > 0.2) & (PSD < 0.3) ].count())/(len(PSD)*100))
+Weights.append((PSD[(PSD > 0.3) & (PSD < 0.4) ].count())/(len(PSD)*100))
+Weights.append((PSD[(PSD > 0.4) & (PSD < 0.5) ].count())/(len(PSD)*100))
+Weights.append((PSD[(PSD > 0.5) & (PSD < 0.6) ].count())/(len(PSD)*100))
+Weights.append((PSD[(PSD > 0.6) & (PSD < 0.7) ].count())/(len(PSD)*100))
+Weights.append((PSD[(PSD > 0.7) & (PSD < 0.8) ].count())/(len(PSD)*100))
+Weights.append((PSD[(PSD > 0.8) & (PSD < 0.9) ].count())/(len(PSD)*100))
+Weights.append((PSD[(PSD > 0.9) & (PSD < 1.0) ].count())/(len(PSD)*100))
+Weights.append((PSD[(PSD > 1.0) & (PSD < 2.0) ].count())/(len(PSD)*100))
+Weights.append((PSD[(PSD > 2.0) & (PSD < 3.0) ].count())/(len(PSD)*100))
+Weights.append((PSD[(PSD > 3.0) & (PSD < 4.0) ].count())/(len(PSD)*100))
+Weights.append((PSD[(PSD > 4.0) & (PSD < 5.0) ].count())/(len(PSD)*100))
+Weights.append((PSD[(PSD > 5.0) & (PSD < 10) ].count())/(len(PSD)*100))
+Weights.append((PSD[(PSD > 10) & (PSD < 20) ].count())/(len(PSD)*100))
+Weights.append((PSD[(PSD > 20) & (PSD < 30) ].count())/(len(PSD)*100))
+Weights.append((PSD[(PSD > 30) & (PSD < 40) ].count())/(len(PSD)*100))
+
+Weights = np.array(Weights)
+
+np.savetxt('PSD_Weights.csv',Weights)
--- GRIS_Minerals_Mie/NetCDF_updater.m
+++ GRIS_Minerals_Mie/NetCDF_updater.m
@@ -8,21 +8,21 @@
 % Joseph Cook, Feb 2017, University of Sheffield, UK.
 
 
-filename = '/home/joe/Code/BioSNICAR_GO/Algal_Optical_Props/snw_alg_40.nc';
+filename = '/home/joe/Code/BioSNICAR_GO/GRIS_Minerals_Mie/GRISdust_PSD.nc';
 rr(1:200) = 0.002; %radius in m
 
-ncwrite(filename,'rds',rr) % particle radius
+%ncwrite(filename,'rds',rr) % particle radius
 %ncwrite(filename,'ext_xsc',ExtXC) % extinction cross section
 %ncwrite(filename,'sca_xsc',ScaXC) % scattering cross section
 %ncwrite(filename,'abs_xsc',AbsXC) % absorption cross section
-ncwrite(filename,'ext_cff_mss',ExtXCmass) % mass extinction cross section
+ncwrite(filename,'ext_cff_mss',weighted_ExtXCmass) % mass extinction cross section
 %ncwrite(filename,'sca_cff_mss',ScaXCmass) % mass scattering cross section
 %ncwrite(filename,'abs_cff_mss',AbsXCmass) % mass absorption cross section
 %ncwrite(filename,'ext_cff_vlm',ExtXCvol) % volume extinction cross section
 %ncwrite(filename,'sca_cff_vlm',ScaXCvol) % volume scattering cross section
 %ncwrite(filename,'abs_cff_vlm',AbsXCvol) % volume absorption cross section
-ncwrite(filename,'ss_alb',ssa) % single scattering albedo
-ncwrite(filename,'asm_prm',asymmetry) % assymetry parameter
+ncwrite(filename,'ss_alb',weighted_ssa) % single scattering albedo
+ncwrite(filename,'asm_prm',weighted_asymmetry) % assymetry parameter
 ncwrite(filename,'rds_swa',1.00001) % surface weighted radius (analytic)
 ncwrite(filename,'rds_swr',1.00001) % surface weighted radius (resolved)
 ncwrite(filename,'rds_nma',1) % analytic number-mean radius
